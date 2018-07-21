3df2c70b05b99bc1cc9cb3b257bda92e9bcaefc6
samhalder59@gmail.com <sambithalder270@gmail.com>
2018-07-21 18:23:32 +0530

    Done on 21st July

---
+++ Vector/Vector.R
@@ -0,0 +1,17 @@
+#Data Structures - Vector
+
+x = c(1,4,3,7,8)
+
+x2 = c(2,5,7,8,4)
+
+x
+x2
+
+x4= c('M', 'F', 'M', 'F', 'M')
+x4
+
+(x5 = 1:100)
+(x6 =seq(1,100,by=3))
+?seq
+
+
--- file2.R
+++ file2.R
@@ -1,1 +1,2 @@
-women
+#Data Structure - Vectors
+
